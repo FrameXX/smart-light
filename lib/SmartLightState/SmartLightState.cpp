c764104a8ae6528cb8627492a7698350249d2a7f
FrameXX <jirkakral978@gmail.com>
2024-01-22 21:47:55 +0100

    Add toMessageCommand method

--- lib/SmartLightState/SmartLightState.cpp
+++ lib/SmartLightState/SmartLightState.cpp
@@ -1,3 +1,15 @@
 #include <SmartLightState.h>
 
 SmartLightState::SmartLightState() {}
+
+MessageCommand SmartLightState::toMessageCommand() const
+{
+  const MessageCommand state(1,
+                             {
+                                 int(this->enabled),
+                                 int(this->RDutyCycle),
+                                 int(this->GDutyCycle),
+                                 int(this->BDutyCycle),
+                             });
+  return state;
+}
--- lib/SmartLightState/SmartLightState.h
+++ lib/SmartLightState/SmartLightState.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <MessageCommand.h>
+
 class SmartLightState
 {
 public:
@@ -9,4 +11,6 @@ public:
   unsigned char BDutyCycle = 255;
 
   SmartLightState();
+
+  MessageCommand toMessageCommand() const;
 };
